1bda01dd40cc1e4e51ca14be35f5f2aecc159fd6
kousar-21 <kousarahammed5@gmail.com>
2025-03-20 19:38:28 +0600

    first commit

---
+++ storage.js
@@ -0,0 +1,12 @@
+
+// getItem always take one para metar and it's as a string
+// setItaem Always take two para metar and it's as a string
+
+const age = 20;
+
+// to set in application in browser
+localStorage.setItem("myAge",age)
+
+
+// to see in console
+console.log(localStorage.getItem("myAge"))
