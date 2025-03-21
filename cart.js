d4dd33c061f2ab3ab41ee918633f5c252b36995f
kousar-21 <kousarahammed5@gmail.com>
2025-03-21 12:10:35 +0600

    add fhk lk

--- cart.js
+++ cart.js
@@ -8,7 +8,8 @@ const handleAddToCart = () => {
 
     // console.log(name, quantity)
     displayCart(name, quantity)
-    handleSetToLocalStorage(name, quantity)
+    // handleSetToLocalStorage(name, quantity)
+    saveLocalStorage(name, quantity)
 
     productName.value = "";
     productQuantity.value="";
@@ -23,10 +24,64 @@ const displayCart = (product,quantity) => {
     productContainer.appendChild(li)
 }
 
-const handleSetToLocalStorage = (productName,Quantity) => {
-    const newProduct = {name: Quantity};
-    console.log(newProduct)
-    localStorage.setItem("cart",JSON.stringify(newProduct))
+
+const getProduct = () => {
+    let cart= {};
+    const gotProduct = localStorage.getItem("cart");
+
+    if(gotProduct){
+        cart = JSON.parse(gotProduct)
+    }
+    
+    return cart;
+    
 
 }
+
+
+const saveLocalStorage = (productName,Quantity) => {
+    const cart =getProduct();
+    console.log(cart);
+
+    // bracket notation concept
+    cart[productName]=Quantity;
+
+    const cartString = JSON.stringify(cart);
+    localStorage.setItem("cart",cartString);
+
+}
+
+const displayFromLocal = () => {
+    const prodocts = getProduct();
+    console.log(prodocts)
+    for(const product in prodocts){
+        console.log(product);
+        console.log(prodocts[product])
+
+        displayCart(product,prodocts[product]);
+    }
+}
+
+displayFromLocal();
+
+
+
+// bracket notation concept
+// const product = {
+//     goals: 20
+// }
+
+// product["Asists"]=5
+// product["goals"]=35
+
+// console.log(product)
+
+
+
+// const handleSetToLocalStorage = (productName,Quantity) => {
+//     const newProduct = {name: Quantity};
+//     console.log(newProduct)
+//     localStorage.setItem("cart",JSON.stringify(newProduct))
+
+// }
 
